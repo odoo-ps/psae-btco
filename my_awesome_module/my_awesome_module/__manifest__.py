298624d9ef154d2d7ef220846b56070a6dfd4d7c
abfa-odoo <abfa@odoo.com>
2023-01-02 18:09:13 +0400

    chapter 5: added new fields, a related field, a model constraint, and added fields to views along with an alert message in product form view

---
+++ my_awesome_module/my_awesome_module/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "my_awesome_module",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        'views/res_partner_views.xml',
+        'views/product_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'license': 'OEEL-1',
+}
